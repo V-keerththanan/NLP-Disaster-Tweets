b628eb4c0e30011ef0d2272d30243925a34122cb
V-keerththanan <keerththananphy@gmail.com>
2022-10-30 17:23:11 +0530

    updated

--- WebApp.py
+++ WebApp.py
@@ -1,7 +1,10 @@
+
 from flask import Flask,render_template,request
+import pickle
 app=Flask(__name__)
-
-
+process=pickle.load(open("Model/processing.pkl","rb"))
+idf=pickle.load(open("Model/TF_IDF.pkl","rb"))
+model=pickle.load(open("Model/LogisticModel.pkl","rb"))
 @app.route("/")
 @app.route("/home")
 def Home():
@@ -9,6 +12,10 @@ def Home():
 @app.route("/predict",methods=['POST'])
 def predict():
     if request.method=='POST':
+        text=request.form.get('tweet').strip()
+        
+        #processedText=process(text)
+        print(text)
         return render_template("predict.html")
 if __name__=='__main__':
     app.run(debug=True)
